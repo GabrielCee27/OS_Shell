177c9836825c6dff99405a80c7f3baaf41777907
GabrielCee27 <gabrielcee81@gmail.com>
2018-10-11 10:05:19 -0700

    print prompt, exec, and fork

--- shell.c
+++ shell.c
@@ -12,14 +12,69 @@
 #include "history.h"
 #include "timer.h"
 
+void print_prompt(void){
+  //TODO:
+  printf("$ ");
+  fflush(stdout);
+}
+
 int main(void) {
 
-    double start = get_time();
-    print_history();
-    sleep(1);
-    double end = get_time();
+    while(true){
+      //works for now
+      print_prompt();
+      char *line = NULL;
+      size_t line_size = 0;
+      getline(&line, &line_size, stdin);
+      printf("-> %s", line);
 
-    printf("Time elapsed: %fs\n", end - start);
+      //TODO: save full command line for history
+      char *token = strtok(line, " \t\n\r");
+      //TODO:research ARG_MAX to find the max num of
+      // args possible for a command
+      char *commands[10];
+
+      //TODO: inspect first element to check
+      // if built in or blank
+      int i = 0;
+      while(token != NULL){
+        commands[i++] = token;
+        token = strtok(NULL, " \t\n\r");
+      }
+
+      if(strcmp(commands[0], "exit") == 0){
+        return 0;
+      }
+
+      //execvp needs last element to be null
+      commands[i] = (char *) NULL;
+
+      //TODO: check err status of fork and exec
+      pid_t pid = fork();
+      if(pid == 0){
+        //child
+
+        printf("Executing: %s", line);
+
+        //recommended to use execvp
+        //since we are gonna have an array of arguments
+        execvp(commands[0], commands);
+      }
+      else{
+        //parent
+        //waits for child to finish
+        int status;
+        wait(&status); //waits for any child to finish
+        printf("Child exited. Status: %d\n", status);
+      }
+    }
+
+    // double start = get_time();
+    // print_history();
+    // sleep(1);
+    // double end = get_time();
+    //
+    // printf("Time elapsed: %fs\n", end - start);
 
     return 0;
 }
